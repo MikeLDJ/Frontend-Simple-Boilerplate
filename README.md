8c4dc9c661da7b17b95ae1be10c6e833d0ce3184
MikeLDJ <mikeu76@gmail.com>
2017-02-28 00:34:43 +0100

    everything works ok

--- README.md
+++ README.md
@@ -1,2 +1,96 @@
 # Frontend-Simple-Boilerplate
-gulp + browsersync + scss + html partials + autoprefixer + uglify
+
+***
+
+### Include:
+
+- gulp
+- browsersync
+- scss
+- html partials
+- autoprefixer
+- cssnano
+- uglify
+- reset CSS
+
+***
+### Directory structure:
+
+```
+./
+├── src/
+│   ├── components/
+│   │   ├── layout/
+│   │   │   ├── _home.html
+│   │   │   ├── _header.html
+│   │   │   └── _footer.html
+│   │   └── stylesheet/
+│   │       ├── base/
+│   │       │   ├── _base.scss
+│   │       │   └── _typography.scss
+│   │       ├── layout/
+│   │       │   ├── _home.scss
+│   │       │   ├── _header.scss
+│   │       │   └── _footer.scss
+│   │       ├── modules/
+│   │       │   ├── _button.scss
+│   │       │   └── _form.scss
+│   │       ├── tools/
+│   │       │   ├── _helpers.scss
+│   │       │   ├── _media-queries.scss
+│   │       │   ├── _mixins.scss
+│   │       │   └── _reset.scss
+│   │       │   └── _variables.scss
+│   │       └── main.scss
+│   ├── css/
+│   ├── fonts/
+│   ├── images/
+│   ├── js/
+│   └── index.html
+│
+├── dev/
+└── prod/
+```
+
+***
+### Require:
+
+- NodeJS
+
+- gulp
+```
+npm install -g gulp
+```
+or
+```
+sudo npm install -g gulp
+```
+
+***
+### Install:
+
+```
+npm install
+```
+
+***
+## Use:
+
+***
+### development:
+
+```
+gulp serve
+```
+
+open browser on ```localhost:3000```
+
+files goes to ```dev``` directiory
+
+### production:
+
+```
+gulp
+```
+
+files in ```prod``` directory
---
+++ gulpfile.js
@@ -0,0 +1,92 @@
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    del = require('del'),
+    injectPartials = require('gulp-inject-partials'),
+    browserSync = require('browser-sync').create(),
+    autoprefixer = require('gulp-autoprefixer'),
+    cssnano = require('gulp-cssnano'),
+    uglify = require('gulp-uglify');
+
+gulp.task('clean', function() {
+  del.sync('./prod/**/*');
+});
+
+gulp.task('sass', function() {
+  gulp.src('src/components/stylesheet/**/*.scss')
+      .pipe(sass().on('error', sass.logError))
+      .pipe(gulp.dest('src/css'))
+      .pipe(browserSync.reload({
+        stream: true
+      }));
+});
+
+gulp.task('copyFiles', function() {
+  return gulp.src(['src/css/**/*', 'src/fonts/**/*', 'src/js/**/*', 'src/images/**/*', 'src/components/layout/**/*'], {
+    base: 'src'
+  }).pipe(gulp.dest('prod'));
+});
+
+gulp.task('browserSync', function() {
+  browserSync.init({
+    browser: ['chrome'],
+    server: {baseDir: 'dev'}
+  });
+});
+
+gulp.task('minCss', ['sass'], function() {
+  return gulp.src('src/css/*.css')
+              .pipe(autoprefixer({
+                browsers: ['last 10 versions'],
+                cascade: false
+              }))
+              .pipe(cssnano())
+              .pipe(gulp.dest('dev/css'));
+});
+
+gulp.task('minJs', function() {
+  return gulp.src('src/js/*.js')
+              .pipe(uglify())
+              .pipe(gulp.dest('dev/js'));
+});
+
+gulp.task('copyDevFiles', ['sass'], function() {
+  gulp.src('src/css/*.css')
+        .pipe(autoprefixer({
+          browsers: ['last 10 versions'],
+          cascade: false
+        }))
+        .pipe(gulp.dest('dev/css'));
+
+  gulp.src('src/js/*.js').pipe(gulp.dest('dev/js'));
+  gulp.src('src/images/**/*').pipe(gulp.dest('dev/images'));
+  gulp.src('src/fonts/**/*').pipe(gulp.dest('dev/fonts'));
+});
+
+gulp.task('buildHTML', function() {
+  return gulp.src('src/*.html')
+              .pipe(injectPartials({
+                removeTags: true
+              }))
+              .pipe(gulp.dest('dev'))
+              .pipe(browserSync.reload({
+                stream: true
+              }));
+});
+
+gulp.task('default', ['clean', 'minCss', 'minJs', 'copyFiles'], function() {
+  return gulp.src('src/index.html')
+              .pipe(injectPartials({
+                removeTags: true
+              }))
+              .pipe(gulp.dest('dev'));
+});
+
+gulp.task('serve', ['clean', 'copyDevFiles', 'copyFiles', 'buildHTML', 'browserSync', 'sass'], function() {
+  gulp.watch('src/**/*.{js,svg,png,jpg,jpeg}', ['copyFiles', 'buildHTML'], function() {
+    browserSync.reload();
+  });
+  gulp.watch('src/**/*.html', ['buildHTML'], function() {
+    browserSync.reload();
+  });
+  gulp.watch('src/**/*.{css,scss}', ['copyDevFiles']);
+});
